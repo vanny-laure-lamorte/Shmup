207f3f0f0350296f29f0aa77437221477bbeb88d
vanny-laure-lamorte <vanny.lamorte@laplateforme.io>
2024-04-03 23:06:34 +0200

    [Ajouter] les options du menu

--- src/gui/Menu.py
+++ src/gui/Menu.py
@@ -30,6 +30,18 @@ class Menu (Element):
         self.text_not_center(self.font5, 60,"Elemental Guardians", self.white, 670, 35)
         self.text_not_center(self.font5, 45,"Mage & Wyrm", self.white, 780, 85)
 
+        # Name        
+        self.img_txt_hover("name", "ENTER YOUR NAME", self.W//2+270, 270, 280, 84, self.images["rect_name"], self.images["rect_name"], self.font2,20, self.white, self.W//2+270,270)    
+
+        # Play
+        self.img_txt_hover("play", "PLAY",self.W//2+270, 365, 280, 94, self.images["rect_menu"], self.images["rect_menu"], self.font2,20, self.white, self.W//2+270, 365)
+    
+        # Options
+        self.img_txt_hover("options", "OPTIONS",self.W//2+270, 445, 280, 94, self.images["rect_menu"], self.images["rect_menu"], self.font2,20, self.white, self.W//2+270, 445)
+
+        # Exit
+        self.img_txt_hover("exit", "EXIT",self.W//2+270, 525, 280, 94, self.images["rect_menu"], self.images["rect_menu"], self.font2,20, self.white, self.W//2+270, 525)
+
        
         # Copyright
         self.text_not_center(self.font, 15,"©", self.white, 15, 677)
