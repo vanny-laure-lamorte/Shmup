df2d6c3ffe4c045d6b2de4f453595d1cd411def0
Lucas Martinie de Maisonneuve <lucas.martinie-de-maisonneuve@laplateforme.io>
2024-04-04 11:07:36 +0200

    [Ajouter] Dragon avec deplacement et animation attaque

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 from src.gui.Menu import Menu
+from src.gui.Game import Game
+from src.gui.Scenario import Scenario
 
-m = Menu()
-m.menu_run()
+# m = Menu()
+# m.menu_run()
+
+# h = HomePage()
+# h.run()
+
+s = Scenario()
+s.run()
